e9532afea2d6e19c2120096627cb57481be8e878
Glitch2210 <MichaelSciortino89@gmail.com>
2025-06-28 09:58:17 -0400

    add cricle shape

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    dt = 0
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
@@ -22,8 +25,12 @@ def main():
         screen.fill(color)
 
 
-
+        #FPS throtal
+        dt =  clock.tick(60) / 1000
+        print(dt)
         pygame.display.flip()
+        
+        
 
 if __name__ == "__main__":
     main()
