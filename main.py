cb747f73e09e43708c1d2260d77ad0cf7cf92715
Glitch2210 <MichaelSciortino89@gmail.com>
2025-06-28 09:16:16 -0400

    added game loop

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #print("Starting Asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        #check for exit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        #local variables
+        color = [0,0,0]
+
+        #Screen Display
+        screen.fill(color)
+
+
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
